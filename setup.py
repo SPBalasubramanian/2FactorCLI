85512ca3ef6a3f1247291e341398af930e4c8852
Rob Smith <kormoc@gmail.com>
2014-07-28 23:45:18 -0700

    setup fixes

--- setup.py
+++ setup.py
@@ -1,28 +1,35 @@
 #!/usr/bin/env python3
 # coding=utf-8
 
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 from pip.req import parse_requirements
 
-install_reqs = parse_requirements('requirements.txt')
-reqs = [str(ir.req) for ir in install_reqs]
-
-setup(
-    name='2factorcli',
-    version='0.0.1',
-    description='This is a simple python program to allow you to store and generate time-based one-time passwords in a GPG encrypted vault.',
-    long_description=open('README.md').read(),
-    author='Rob Smith',
-    author_email='kormoc@gmail.com',
-    url='https://github.com/kormoc/2FactorCLI',
-    packages=[],
-    scripts=['2factorcli.py'],
-    license='MIT',
-    install_requires=reqs,
-    classifiers=[
+config = {
+    'name': '2factorcli',
+    'version': '0.0.2',
+    'description': 'This is a simple python program to allow you to store and '
+    ' generate time-based one-time passwords in a GPG encrypted vault.',
+    'author': 'Rob Smith',
+    'author_email': 'kormoc@gmail.com',
+    'url': 'https://github.com/kormoc/2FactorCLI',
+    'scripts': ['2factorcli.py'],
+    'license': 'MIT',
+    'classifiers': [
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
     ],
-)
+}
+
+with open('README.md', 'r') as fp:
+    config['long_description'] = fp.read()
+
+install_reqs = parse_requirements('requirements.txt')
+config['install_requires'] = [str(ir.req) for ir in install_reqs]
+
+setup(**config)
